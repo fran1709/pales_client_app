b1ac71d123873e3ab20a97fa4e6c3add779fc42f
fran1709 <fran1709@estudiantec.cr>
2023-10-10 20:17:54 -0600

    agregar nuevos comentarios funcional

--- app/src/main/java/com/example/client_app/ReseniasActivity.kt
+++ app/src/main/java/com/example/client_app/ReseniasActivity.kt
@@ -1,12 +1,16 @@
 package com.example.client_app
 
 import android.content.ContentValues.TAG
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
+import com.google.firebase.firestore.FieldValue
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,6 +18,7 @@ import com.google.firebase.Timestamp
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.gson.Gson
+import java.util.Calendar
 
 data class Resenia(
     val comentario: String,
@@ -26,6 +31,7 @@ class ReseniasActivity : AppCompatActivity() {
     val db = Firebase.firestore
     val reseniasList = ArrayList<Resenia>()
     private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: ReseniasAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resenias)
@@ -35,6 +41,7 @@ class ReseniasActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "Pale's Reseñas"
+
 
         // Inicializar el RecyclerView
         recyclerView = findViewById(R.id.recyclerView)
@@ -49,19 +56,70 @@ class ReseniasActivity : AppCompatActivity() {
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.action_search -> {
-                // Acción para el elemento de búsqueda
-                Toast.makeText(this, "Elemento de búsqueda seleccionado", Toast.LENGTH_SHORT).show()
-                true
-            }
-            R.id.action_settings -> {
+            R.id.nuevoComentario -> {
                 // Acción para el elemento de configuración
-                Toast.makeText(this, "Elemento de configuración seleccionado", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Agregar nuevo comentario seleccionado", Toast.LENGTH_SHORT).show()
+                showComentarioDialog();
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    fun createComentario(comentario: String, estado: Boolean, jugador: String) {
+        // Crear un nuevo documento en la colección "resena"
+        val nuevoComentario = hashMapOf(
+            "comentario" to comentario,
+            "estado" to estado,
+            "jugador" to jugador,
+            "fecha" to FieldValue.serverTimestamp() // Agregar la fecha actual del servidor
+        )
+
+        db.collection("resena")
+            .add(nuevoComentario)
+            .addOnSuccessListener { documentReference ->
+                // El comentario se creó con éxito, puedes mostrar un mensaje o realizar otras acciones
+                Toast.makeText(this@ReseniasActivity, "Comentario creado con éxito", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+                // Error al crear el comentario, muestra un mensaje de error o realiza acciones de manejo de errores
+                Log.w(TAG, "Error al crear el comentario", e)
+                Toast.makeText(this@ReseniasActivity, "Error al crear el comentario", Toast.LENGTH_SHORT).show()
+            }
+        // Obtener la fecha actual
+        val calendar = Calendar.getInstance()
+
+        // Crear un objeto Timestamp a partir de la fecha actual
+        val fechaActual = Timestamp(calendar.time)
+        reseniasList.add(Resenia(comentario, estado, fechaActual, jugador));
+        adapter.notifyDataSetChanged();
+    }
+
+    fun showComentarioDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Agregar Comentario")
+
+        val input = EditText(this)
+        input.hint = "Escribe tu comentario aquí"
+        builder.setView(input)
+
+        builder.setPositiveButton("Aceptar") { dialog: DialogInterface, _ ->
+            val comentario = input.text.toString()
+            if (comentario.isNotEmpty()) {
+                // TODO: Se debe de obtener el id del jugador para guardarlo en el param @jugador
+                createComentario(comentario, true, "Nombre del Jugador")
+            }
+            dialog.dismiss()
+        }
+
+        builder.setNegativeButton("Cancelar") { dialog: DialogInterface, _ ->
+            dialog.dismiss()
+        }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+
     fun getResenias() {
         db.collection("resena")
             .get()
@@ -81,17 +139,17 @@ class ReseniasActivity : AppCompatActivity() {
                     Toast.makeText(this@ReseniasActivity, "Sí hay comentarios disponibles", Toast.LENGTH_SHORT).show()
 
                     // Convierte la lista de resenias a JSON
-                    val gson = Gson()
-                    val jsonData = gson.toJson(reseniasList)
+                    //val gson = Gson()
+                    //val jsonData = gson.toJson(reseniasList)
                     // Imprime el JSON en el logcat
-                    Log.d("JSON", jsonData) // "JSON" es un identificador para encontrar más fácilmente el registro en logcat
+                    //Log.d("JSON", jsonData) // "JSON" es un identificador para encontrar más fácilmente el registro en logcat
 
                     // La lista reseniasList se llena con comentarios
                     //chargeListViewCompleja()
                     //chargeListViewSimple()
 
                     // Agregar adaptador al RecyclerView
-                    val adapter = ReseniasAdapter(reseniasList)
+                    this.adapter = ReseniasAdapter(reseniasList)
                     recyclerView.adapter = adapter
 
                 } else {
