e3a167a7bf9272d954e6d74f1ebc10bb5b11b3ae
María Belén Miranda Hernandez <61504373+Belen-miranda11@users.noreply.github.com>
2023-10-23 15:37:02 -0600

    Cambios

--- app/src/main/java/com/example/client_app/MainActivity.kt
+++ app/src/main/java/com/example/client_app/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.example.client_app
 
 import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
