db9c5a408620a1fd1d48b9e155d0fd668d46ce0f
María Belén Miranda Hernandez <61504373+Belen-miranda11@users.noreply.github.com>
2023-10-24 20:57:18 -0600

    Aplicar Auth.CurrentUser

--- app/src/main/java/com/example/client_app/MiPerfil.kt
+++ app/src/main/java/com/example/client_app/MiPerfil.kt
@@ -9,23 +9,29 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
+import com.google.firebase.ktx.Firebase
 
 private lateinit var startForResult: ActivityResultLauncher<Intent>
 
 class MiPerfil : AppCompatActivity() {
     private lateinit var db: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mi_perfil)
         // Inicializar Firebase
         db = FirebaseFirestore.getInstance()
+        auth = Firebase.auth
 
         val editProfile: ImageButton = findViewById(R.id.editProfile)
 
-        userData()
-        val id = "h5GdAbTqbDOT8fbqX6GwIGGFokw1"
+        val id = auth.currentUser?.uid
+
+        userData(id)
 
         editProfile.setOnClickListener {
             callActivityEditar(id)
@@ -56,7 +62,7 @@ class MiPerfil : AppCompatActivity() {
                 document.update("nombre", nombre1.toString())
                     .addOnSuccessListener {
                         // Éxito, los datos se han actualizado correctamente
-                        userData()
+                        userData(id)
                     }
                     .addOnFailureListener { exception ->
                         // Maneja cualquier error
@@ -64,7 +70,7 @@ class MiPerfil : AppCompatActivity() {
                 document.update("apodo", apodo1.toString())
                     .addOnSuccessListener {
                         // Éxito, los datos se han actualizado correctamente
-                        userData()
+                        userData(id)
                     }
                     .addOnFailureListener { exception ->
                         // Maneja cualquier error
@@ -72,7 +78,7 @@ class MiPerfil : AppCompatActivity() {
                 document.update("fecha_nacimiento", age1.toString())
                     .addOnSuccessListener {
                         // Éxito, los datos se han actualizado correctamente
-                        userData()
+                        userData(id)
                     }
                     .addOnFailureListener { exception ->
                         // Maneja cualquier error
@@ -80,7 +86,7 @@ class MiPerfil : AppCompatActivity() {
                 document.update("telefono", phone1.toString())
                     .addOnSuccessListener {
                         // Éxito, los datos se han actualizado correctamente
-                        userData()
+                        userData(id)
                     }
                     .addOnFailureListener { exception ->
                         // Maneja cualquier error
@@ -89,7 +95,7 @@ class MiPerfil : AppCompatActivity() {
         }
     }
 
-    private fun userData() {
+    private fun userData(id : String?) {
         val userName: TextView = findViewById(R.id.tvUsers)
         val nickname: TextView = findViewById(R.id.nicknameText)
         val correoText: TextView = findViewById(R.id.correoText)
@@ -98,37 +104,39 @@ class MiPerfil : AppCompatActivity() {
         val phone: TextView = findViewById(R.id.telefonoText)
         val clasificacion: TextView = findViewById(R.id.clasificacionText)
 
-        val id = "h5GdAbTqbDOT8fbqX6GwIGGFokw1"
 
-       db.collection("jugadores")
-            .whereEqualTo("UID", id)
-            .get()
-            .addOnSuccessListener { documentSnapshot ->
-                for (document in documentSnapshot) {
-                    // Retrieve data from the document
-                    val playerName = document.getString("nombre")
-                    val playerNickname = document.getString("apodo")
-                    val playerPositions = document.get("posiciones") as List<String>?
-                    val playerBirthday = document.getString("fecha_nacimiento")
-                    val playerPhone = document.getString("telefono")
-                    val playerClasificacion = document.getString("clasificacion")
-                    val playerCorreo = document.getString("correo")
+        if (id != null) {
+            db.collection("jugadores")
+                .whereEqualTo("UID", id)
+                .get()
+                .addOnSuccessListener { documentSnapshot ->
+                    for (document in documentSnapshot) {
+                        // Retrieve data from the document
+                        val playerName = document.getString("nombre")
+                        val playerNickname = document.getString("apodo")
+                        val playerPositions = document.get("posiciones") as List<String>?
+                        val playerBirthday = document.getString("fecha_nacimiento")
+                        val playerPhone = document.getString("telefono")
+                        val playerClasificacion = document.getString("clasificacion")
+                        val playerCorreo = document.getString("correo")
 
-                    // Update the TextViews with the retrieved data
-                    userName.text = "$playerName"
-                    nickname.text = "$playerNickname"
-                    position.text = "${playerPositions?.joinToString(", ")}"
-                    age.text = "$playerBirthday"
-                    phone.text = "$playerPhone"
-                    clasificacion.text = "$playerClasificacion"
-                    correoText.text = "$playerCorreo"
+                        // Update the TextViews with the retrieved data
+                        userName.text = "$playerName"
+                        nickname.text = "$playerNickname"
+                        position.text = "${playerPositions?.joinToString(", ")}"
+                        age.text = "$playerBirthday"
+                        phone.text = "$playerPhone"
+                        clasificacion.text = "$playerClasificacion"
+                        correoText.text = "$playerCorreo"
+                    }
                 }
-            }
-            .addOnFailureListener { exception ->
-                // Handle failures
-            }
+                .addOnFailureListener { exception ->
+                    // Handle failures
+                }
+        }
+
     }
-    fun callActivityEditar(id : String){
+    fun callActivityEditar(id : String?){
         // Crear un Intent para iniciar la Activity
         val intent = Intent(this, EditarPerfil::class.java)
         intent.putExtra("idUser",id)
